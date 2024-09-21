d2442c8507bae1dd3de71f5dce7b5e3994877663
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2024-09-21 22:19:03 +0200

    Update migration

--- server/persist/migrations/01_oculite.sql
+++ server/persist/migrations/01_oculite.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 16.3
 
--- Started on 2024-09-15 21:09:41
+-- Started on 2024-09-21 22:18:48
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -45,8 +45,10 @@ ALTER TABLE public._sqlx_migrations OWNER TO postgres;
 --
 
 CREATE TABLE public.categories (
-    category character varying NOT NULL,
-    url character varying NOT NULL
+    url character varying NOT NULL,
+    category_id integer NOT NULL,
+    config character varying,
+    category_name character varying NOT NULL
 );
 
 
@@ -64,11 +66,27 @@ CREATE TABLE public.item_trackers (
     icon text,
     link character varying,
     favorite boolean,
-    category character varying NOT NULL
+    category_id integer NOT NULL
 );
 
 
 ALTER TABLE public.item_trackers OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 24616)
+-- Name: notifications; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.notifications (
+    id integer NOT NULL,
+    message character varying NOT NULL,
+    icon character varying NOT NULL,
+    color character varying NOT NULL,
+    date timestamp with time zone NOT NULL
+);
+
+
+ALTER TABLE public.notifications OWNER TO postgres;
 
 --
 -- TOC entry 216 (class 1259 OID 16402)
@@ -77,7 +95,7 @@ ALTER TABLE public.item_trackers OWNER TO postgres;
 
 CREATE TABLE public.price_data (
     item_tracker integer NOT NULL,
-    date date NOT NULL,
+    date timestamp with time zone NOT NULL,
     price double precision NOT NULL
 );
 
@@ -85,7 +103,7 @@ CREATE TABLE public.price_data (
 ALTER TABLE public.price_data OWNER TO postgres;
 
 --
--- TOC entry 3407 (class 0 OID 16394)
+-- TOC entry 3413 (class 0 OID 16394)
 -- Dependencies: 215
 -- Data for Name: _sqlx_migrations; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -95,32 +113,37 @@ COPY public._sqlx_migrations (version, description, installed_on, success, check
 
 
 --
--- TOC entry 3406 (class 0 OID 16387)
+-- TOC entry 3412 (class 0 OID 16387)
 -- Dependencies: 214
 -- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.categories (category, url) FROM stdin;
-bla	test.com
-test	test.com
-blabla	test.com
-omg what	test.com
-omg	test.com
+COPY public.categories (url, category_id, config, category_name) FROM stdin;
 \.
 
 
 --
--- TOC entry 3409 (class 0 OID 16407)
+-- TOC entry 3415 (class 0 OID 16407)
 -- Dependencies: 217
 -- Data for Name: item_trackers; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.item_trackers (id, name, currency, icon, link, favorite, category) FROM stdin;
+COPY public.item_trackers (id, name, currency, icon, link, favorite, category_id) FROM stdin;
 \.
 
 
 --
--- TOC entry 3408 (class 0 OID 16402)
+-- TOC entry 3416 (class 0 OID 24616)
+-- Dependencies: 218
+-- Data for Name: notifications; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.notifications (id, message, icon, color, date) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3414 (class 0 OID 16402)
 -- Dependencies: 216
 -- Data for Name: price_data; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -130,7 +153,7 @@ COPY public.price_data (item_tracker, date, price) FROM stdin;
 
 
 --
--- TOC entry 3257 (class 2606 OID 16401)
+-- TOC entry 3261 (class 2606 OID 16401)
 -- Name: _sqlx_migrations _sqlx_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -139,16 +162,16 @@ ALTER TABLE ONLY public._sqlx_migrations
 
 
 --
--- TOC entry 3255 (class 2606 OID 16393)
+-- TOC entry 3259 (class 2606 OID 16429)
 -- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT categories_pkey PRIMARY KEY (category);
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (category_id);
 
 
 --
--- TOC entry 3261 (class 2606 OID 16413)
+-- TOC entry 3265 (class 2606 OID 16413)
 -- Name: item_trackers item_trackers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -157,7 +180,16 @@ ALTER TABLE ONLY public.item_trackers
 
 
 --
--- TOC entry 3259 (class 2606 OID 16406)
+-- TOC entry 3267 (class 2606 OID 24622)
+-- Name: notifications notifications_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.notifications
+    ADD CONSTRAINT notifications_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3263 (class 2606 OID 24624)
 -- Name: price_data price_data_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -166,16 +198,16 @@ ALTER TABLE ONLY public.price_data
 
 
 --
--- TOC entry 3263 (class 2606 OID 16414)
+-- TOC entry 3269 (class 2606 OID 16430)
 -- Name: item_trackers category; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.item_trackers
-    ADD CONSTRAINT category FOREIGN KEY (category) REFERENCES public.categories(category);
+    ADD CONSTRAINT category FOREIGN KEY (category_id) REFERENCES public.categories(category_id) NOT VALID;
 
 
 --
--- TOC entry 3262 (class 2606 OID 16419)
+-- TOC entry 3268 (class 2606 OID 16419)
 -- Name: price_data item_tracker; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -183,7 +215,7 @@ ALTER TABLE ONLY public.price_data
     ADD CONSTRAINT item_tracker FOREIGN KEY (item_tracker) REFERENCES public.item_trackers(id) NOT VALID;
 
 
--- Completed on 2024-09-15 21:09:41
+-- Completed on 2024-09-21 22:18:48
 
 --
 -- PostgreSQL database dump complete
