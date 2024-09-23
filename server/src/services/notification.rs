ade943e3e4c10bfcded11521efc983a8d9d22424
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2024-09-23 22:22:07 +0200

    Add function signatures for notifications

--- server/src/services/notification.rs
+++ server/src/services/notification.rs
@@ -3,3 +3,24 @@
 // Category not responding notification
 // Category added notification
 // Inactive/old trackers deletion notification
+
+
+pub async fn insert_login() -> Result<(), sqlx::Error> {
+    todo!("Insert login notification")
+}
+
+pub async fn insert_price_change() -> Result<(), sqlx::Error> {
+    todo!("Insert price change notification")
+}
+
+pub async fn insert_category_not_responding() -> Result<(), sqlx::Error> {
+    todo!("Insert category not responding notification")
+}
+
+pub async fn insert_category_added() -> Result<(), sqlx::Error> {
+    todo!("Insert category added notification")
+}
+
+pub async fn insert_inactive_trackers_deletion() -> Result<(), sqlx::Error> {
+    todo!("Insert inactive/old trackers deletion notification")
+}
