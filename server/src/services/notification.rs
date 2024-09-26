790d17a9e9c895c3cda73c0252af316909351983
Samuel Bruin <samuelmbruin@yahoo.com>
2024-09-26 11:36:31 +0200

    Add notification implementations

--- server/src/services/notification.rs
+++ server/src/services/notification.rs
@@ -16,14 +16,44 @@ pub async fn insert_login(db: &PgPool, ip: &str) -> Result<(), sqlx::Error> {
     Ok(())
 }
 
-pub async fn insert_price_change(db: &PgPool) -> Result<(), sqlx::Error> {
-    todo!("Insert price change notification")
+pub async fn insert_price_change(db: &PgPool, it_name: &str, price_change_percent: f64, new_price: f64, currency: &str) -> Result<(), sqlx::Error> {
+    let message = std::format!(
+        "{} has significantly changed price ({}%). Now {} {} [{}]",
+        it_name,
+        price_change_percent,
+        currency,
+        new_price,
+        Utc::now()
+            .with_timezone(&chrono_tz::Europe::Amsterdam)
+            .format("%Y-%m-%d %H:%M:%S"),
+    );
+    persist::notification::insert(db, &message, "pi-sign-in", "green", Utc::now()).await?;
+
+    Ok(())
 }
 
-pub async fn insert_category_not_responding(db: &PgPool) -> Result<(), sqlx::Error> {
-    todo!("Insert category not responding notification")
+pub async fn insert_category_not_responding(db: &PgPool, category_name: &str) -> Result<(), sqlx::Error> {
+    let message = std::format!(
+        "Category {} is not responding [{}]",
+        category_name,
+        Utc::now()
+            .with_timezone(&chrono_tz::Europe::Amsterdam)
+            .format("%Y-%m-%d %H:%M:%S"),
+    );
+    persist::notification::insert(db, &message, "pi-sign-in", "green", Utc::now()).await?;
+
+    Ok(())
 }
 
-pub async fn insert_category_added(db: &PgPool) -> Result<(), sqlx::Error> {
-    todo!("Insert category added notification")
+pub async fn insert_category_added(db: &PgPool, category_name: &str) -> Result<(), sqlx::Error> {
+    let message = std::format!(
+        "A new category has been added on {}: ",
+        Utc::now()
+            .with_timezone(&chrono_tz::Europe::Amsterdam)
+            .format("%Y-%m-%d %H:%M:%S"),
+        category_name
+    );
+    persist::notification::insert(db, &message, "pi-sign-in", "green", Utc::now()).await?;
+
+    Ok(())
 }
